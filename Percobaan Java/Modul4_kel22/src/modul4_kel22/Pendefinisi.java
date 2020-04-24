e39b7284d2b6849e1bae52057c8973f57975bfc9
Andikaauli <dikaauli98@gmail.com>
2020-04-24 14:23:54 +0700

    Praktikum Modul 4

---
+++ Percobaan Java/Modul4_kel22/src/modul4_kel22/Pendefinisi.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul4_kel22;
+
+/**
+ *
+ * @author User ID
+ */
+public class Pendefinisi {
+    public void greeting(){
+        System.out.println("Hai, saya method dari class pendefinisi. Salam kenal :)");
+    }
+    public void kelompok(String kelompok){
+        System.out.println(kelompok);
+    }
+    public String kenalan (String nama, String hobi){
+        return "Hai, Nama saya " + nama + " hobi saya " + hobi;
+    }
+
+    
+}
